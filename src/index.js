eda68a5bf1427ed875f222da18e0738619645162
iMac <iMac@iMac-iMac.local>
2018-11-30 00:19:22 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -68,7 +68,34 @@ function delegate(target, fn) {
  * @param {Element} target - элемент, на который нужно добавить обработчик
  * @param {function} fn - обработчик
  */
-function once(target, fn) {}
+function once(target, fn) {
+  let handler = fn;
+  target.addEventListener("click", handler);
+  target.addEventListener("click", () => {
+    let clicked = 1;
+    return clicked;
+  });
+  if (clicked == 1) {
+    target.removeEventListener("click", handler);
+  }
+}
+
+const button = document.querySelector("#one");
+function once(target, fn) {
+  let handler = fn;
+  let clicked = 0;
+  target.addEventListener("click", handler);
+  target.addEventListener("click", () => {
+    clicked = 1;
+    return clicked;
+  });
+  if (clicked == 1) {
+    target.removeEventListener("click", handler);
+  }
+}
+once(button, () => {
+  console.log("кнопка нажата");
+});
 
 export {
   addListener,
