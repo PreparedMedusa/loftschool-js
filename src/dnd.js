17e7fae0807513a890cc7385c720bea467037f34
iMac <iMac@iMac-iMac.local>
2019-01-01 17:35:44 +0300

    hw 5 dnd.js

--- src/dnd.js
+++ src/dnd.js
@@ -25,6 +25,22 @@ let homeworkContainer = document.querySelector("#homework-container");
 function createDiv() {
   let div = document.createElement("div");
   div.className = "draggable-div";
+  div.style.backgroundColor = getRandomClr();
+  div.style.left = getRndInteger(0, document.body.clientWidth);
+  div.style.top = getRndInteger(0, window.innerHeight);
+  div.style.width = getRndInteger(1, document.body.clientWidth);
+  div.style.height = getRndInteger(1, window.innerHeight);
+  function getRandomClr() {
+    return (
+      "#" +
+      Math.random()
+        .toString(16)
+        .slice(-6)
+    );
+  }
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min + "px";
+  }
   return div;
 }
 
